cca916b8d4c85552f80fe349b3d9bf9c402a98ee
Margaret Green <margaret@techelevator.com>
2023-08-19 17:13:14 -0400

    removed problem file

--- PokemonFullStackReview/pokemon-server/src/main/java/com/techelevator/pokemon/dao/PokemonDao.java
+++
@@ -1,9 +0,0 @@
-package com.techelevator.pokemon.dao;
-
-import com.techelevator.pokemon.model.Pokemon;
-import com.techelevator.pokemon.model.PokemonDetail;
-
-public interface PokemonDao {
-
-    PokemonDetail savePokemon(PokemonDetail detail);
-}
